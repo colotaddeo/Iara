3724a23119e39eeb9f2791986e065a67f0208ef2
colotaddeo <nicotaddeo7@gmail.com>
2022-09-18 16:41:28 -0300

    lung added
    chau cubito, hola lung.
    Le falta una banda igual

--- Front-End/src/components/pages/landingPage/3dmodel/model.jsx
+++ Front-End/src/components/pages/landingPage/3dmodel/model.jsx
@@ -1,61 +1,56 @@
-import React from 'react'
-import * as THREE from 'three'
-import { useEffect } from 'react'
+import React, { useState, useRef, useEffect } from "react"
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js"
 
-const model = () => {
+
+const Object = () => {
+  const canvasRef = useRef(null)
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
+  const renderer = new THREE.WebGLRenderer({ antialias: true })
+  const controls = new OrbitControls(camera, renderer.domElement)
 
   useEffect(() => {
-    const scene = new THREE.Scene();
-
-    const camera = new THREE.PerspectiveCamera(
-      50,
-      window.innerWidth / window.innerHeight,
-      1,
-      1000
-    );
-    camera.position.z = 96;
-
-    const canvas = document.getElementById ('myThreeJsCanvas');
-    const renderer = new THREE. WebGLRenderer ({
-      canvas,
-      antialias: true,
-    });
-    renderer.setSize(window. innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    ambientLight.castShadow = true;
-    scene.add(ambientLight);
-
-    const spotLight = new THREE.SpotLight(0xffffff, 1);
-    spotLight.castShadow = true;
-    spotLight.position.set(0, 64, 32);
-    scene.add(spotLight);
-
-    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
-    const boxMaterial = new THREE.MeshNormalMaterial();
-    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    scene.add(boxMesh);
-
+    const canvas = canvasRef.current
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    canvas.appendChild(renderer.domElement)
+    camera.position.z = 4;
+    controls.update()
+    const light = new THREE.AmbientLight(0xffffff, 0.5)
+    scene.add(light)
+    const light2 = new THREE.PointLight(0xffffff, 0.5)
+    scene.add(light2)
+    function loadGLTF() {
+      let object = new GLTFLoader()
+      object.load(
+        "./lung.glb",
+        function (gltf) {
+          scene.add(gltf.scene)
+        },
+        function (error) {
+          console.error(error)
+        }
+      )
+    }
+    loadGLTF()
     const animate = () => {
-      boxMesh.rotation.x += 0.01;
-      boxMesh.rotation.y += 0.01;      
-      renderer.render(scene, camera);
-      window.requestAnimationFrame(animate);
-    };
-    animate();
-  }, []);
-
+      requestAnimationFrame(animate)
+      renderer.render(scene, camera)
+    }
+    animate()
+  }, [])
 
   return (
-    <div>
-
-      <canvas id="myThreeJsCanvas"/>
-
+    <div className="container-canvas">
+      <div ref={canvasRef} className="canvas"></div>
     </div>
   )
 }
 
-export default model;
+export default Object
