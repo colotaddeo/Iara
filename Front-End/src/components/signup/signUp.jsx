2831aa21ed94dc49e676c410155bb280deec98e3
colotaddeo <nicotaddeo7@gmail.com>
2022-10-07 20:31:37 -0300

    sign up casi listo, axios integrated

--- Front-End/src/components/signup/signUp.jsx
+++ Front-End/src/components/signup/signUp.jsx
@@ -1,45 +1,89 @@
 import React, {useState} from 'react'
 import './signUp.css'
-import Media from '../Media/Media.jsx'
 import Imagen from '../.././assets/Doctora1.png'
 import {Link} from 'react-router-dom';
-import {Formik, useFormik, Form, Field} from 'formik';
+import {Formik, Form, Field} from 'formik';
+import { TextField, Button} from "@mui/material";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import * as Yup from 'yup'
+import axios from 'axios';
+
 
 
 const signUp = () => {
-
-  const datosSign = [
-    {
-      lbl: "Nombre:",
-      type: "text"
-    },
-    {
-      lbl: "Apellido:",
-      type: "text"
-    },
-    {
-      lbl: "DNI:",
-      type: "text",
-    },
-    {
-      lbl: "E-mail:",
-      type: "email"
-    }
-  ]
-  const Numeros= (string) =>{//Solo numeros
-    var out = '';
-    var filtro = '1234567890';//Caracteres validos
-	
-    //Recorrer el texto y verificar si el caracter se encuentra en la lista de validos 
-    for (var i=0; i<string.length; i++)
-       if (filtro.indexOf(string.charAt(i)) != -1) 
-        //Se añaden a la salida los caracteres validos
-	     out += string.charAt(i);
-	
-    //Retornar valor filtrado
-    return out;
-  } 
   
+  const valoresIniciales = {
+    nombre: '',
+    apellido:'',
+    email: '',
+    contraseña: '',
+    confirmarContraseña: '',
+    hospital: '-1',
+  }
+
+  const validationSchema =  Yup.object({
+    nombre: Yup.string()
+      .required('Campo obligatorio')
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!'),
+    apellido: Yup.string()
+      .required('Campo obligatorio')
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!'),
+    email: Yup.string()
+      .required('Campo obligatorio')
+      .email('Email inválido'),
+    contraseña: Yup.string()
+      .required('Campo obligatorio')
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!'),
+    confirmarContraseña: Yup.string()
+      .required('Campo obligatorio')
+      .oneOf([Yup.ref("contraseña")], "Las contraseñas no coinciden"),
+    hospital: Yup.string()
+      .required('Campo obligatorio'),
+
+
+  })
+
+  const fetchAxios = (valoresIniciales) => {
+
+
+    axios.post("http://localhost:4000/user/signin",{
+
+      body: JSON.stringify(valoresIniciales),
+      headers: {"Content-Type" : "application/json" }
+
+    })
+
+    .catch(error => console.log("Error", error))
+    .then(response => console.log(response))
+  }
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...valoresIniciales, [prop]: event.target.valoresIniciales });
+  };
+
+  // const handleClickShowPassword = () => {
+  //   setValues({
+  //     ...valoresIniciales,
+  //     showPassword: !valoresIniciales.showPassword,
+  //   });
+  // };
+
+  // const handleMouseDownPassword = (event) => {
+  //   event.preventDefault();
+  // };
+
+  const [hospital, sethospital] = React.useState('');
+
+  const handleChangehospital = (event) => {
+    sethospital(event.target.value);
+  };
+
   return (
     <div className="all-container">
 
@@ -51,59 +95,153 @@ const signUp = () => {
 
       <div className="form">
         
-        <h2>Crear cuenta</h2>
+        <h2>Registro</h2>
 
         <Formik 
 
           initialValues={valoresIniciales}
+          validationSchema={validationSchema}
           onSubmit={(valoresIniciales) => {
 
-            fetchinfo(valoresIniciales); 
+            fetchAxios(valoresIniciales); 
             alert(JSON.stringify(valoresIniciales));
-            console.log(valoresIniciales);
+            console.log(valoresIniciales.json);
+            formikHelpers.resetForm();
 
           }}
-          
 
         >
+          {({ errors, isValid, touched, dirty }) => (
           <Form>
+            
             <Field
               id="nombre"
               name="nombre"
               type="text"
-              placeholder="Ingresá tu nombre"
+              as={TextField}
+              variant="outlined"
+              color="primary"
+              label="Nombre"
+              size="normal"
+              error={Boolean(errors.nombre) && Boolean(touched.nombre)}
+              helperText={Boolean(touched.nombre) && errors.nombre}
               className="input__signup"
+              
             />
+
             <Field
               id="apellido"
               name="apellido"
               type="text"
-              placeholder="Ingresa tu apellido"
+              as={TextField}
+              variant="outlined"
+              color="primary"
+              label="Apellido"
+              size="normal"
+              error={Boolean(errors.apellido) && Boolean(touched.apellido)}
+              helperText={Boolean(touched.apellido) && errors.apellido}
               className="input__signup"
             />
+
             <Field
-              id="apellido"
-              name="apellido"
-              type="text"
-              placeholder="Ingresa tu apellido"
+              id="email"
+              name="email"
+              type="email"
               className="input__signup"
+              as={TextField}
+              variant="outlined"
+              color="primary"
+              label="Email"
+              size="normal"
+              error={Boolean(errors.email) && Boolean(touched.email)}
+              helperText={Boolean(touched.email) && errors.email}
             />
-            <button className="button__login" type="submit">Submit</button>
+
+            <Field
+              id="contraseña"
+              name="contraseña"
+              type="password"
+              as={TextField}
+              variant="outlined"
+              color="primary"
+              label="Contraseña"
+              size="normal"
+              error={Boolean(errors.contraseña) && Boolean(touched.contraseña)}
+              helperText={Boolean(touched.contraseña) && errors.contraseña}
+              className="input__signup"
+
+            />
+
+            
+            {/* <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={valoresIniciales.showPassword ? 'text' : 'password'}
+                value={valoresIniciales .password}
+                onChange={handleChange('password')}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {valoresIniciales.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl> */}
+
+            <Field
+              id="confirmarContraseña"
+              name="confirmarContraseña"
+              type="password"
+              as={TextField}
+              variant="outlined"
+              color="primary"
+              label="Confirmar contraseña"
+              size="normal"
+              error={Boolean(errors.confirmarContraseña) && Boolean(touched.confirmarContraseña)}
+              helperText={Boolean(touched.confirmarContraseña) && errors.confirmarContraseña}
+              className="input__signup"
+              
+            />
+
+            <FormControl sx={{ m: 1, minWidth: 200 }}>
+              <InputLabel id="hospital">Hospital Asociado</InputLabel>
+              <Select
+                id="hospital"
+                value= {hospital}
+                label="Hospital Asociado"
+                autoWidth
+                onChange={handleChangehospital}
+              >
+                <MenuItem value="muñiz">Muñiz</MenuItem>
+                <MenuItem value="umai">umai</MenuItem>
+                <MenuItem value="{30}">Thirty</MenuItem>
+              </Select>
+            </FormControl>
+
+            <Button
+              variant="contained"
+              type="submit"
+              size="large"
+              disabled={!dirty || !isValid}
+            >
+              Continuar
+            </Button>
+            
           </Form>
+          )}
+
         </Formik>
 
-        <div className='dropdown-container'>
-
-          <select name="Instituciones" id="">
-            <option value="-1">Institución</option>
-            <option value="Umai">Umai</option>
-            <option value="Muñiz">Muñiz</option>
-          </select>
-        
-        </div>
         <div className='button-container'>
-        <Link to= '/home' className='login-button'>Ya tenes cuenta?</Link>
-        <a href="" className='confirm'>Confirmar</a>
+        <Link to= '/login' className='login-button'>Ya tenes cuenta?</Link>
         </div>
 
       </div>
