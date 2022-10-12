1d034aca693ba793abbe23369e14d4a6708c78db
Benzagon <91753390+Benzagon@users.noreply.github.com>
2022-10-12 14:20:02 -0300

    feat(images and header)

--- Front-End/src/components/pages/landingPage/landingPage.jsx
+++ Front-End/src/components/pages/landingPage/landingPage.jsx
@@ -6,10 +6,26 @@ import Model from'../landingPage/3dmodel/model'
    const HomePage = () => {
   return (
     <div>
-      <Header />
-      <Body />
-      <Model />
+      <header class="header">
+        <nav class="flex flex-jc-sb flex ai-c">
+            
+            <div class="header__links flex flex-ai-c wrapper">
+                <a href="/">
+                    <img src="images/logoTxt.svg" alt="Iara"></img>
+                </a>
+                
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#about_id">Acerca de nosotros</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>        
+            </div>
 
+            <div class ="nav__button">
+                <button type="submit" class="cyanBtn">Ir a Mi Escritorio</button>
+            </div>
+        </nav>
+      </header>
     </div>
   )
 }
