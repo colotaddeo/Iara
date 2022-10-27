cc293c392db6b613e3266fdfbcd0e9c84a33aa8d
LuisEmbonStrizzi <luisembonstrizzi@gmail.com>
2022-10-27 15:31:05 -0300

    feat: Added forgot password function

--- Front-End/src/components/ForgotPassword.jsx
+++ Front-End/src/components/ForgotPassword.jsx
@@ -1,8 +1,33 @@
-const Forgot_password=() => {
-    
+import { Form, Formik, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useImages } from "../hooks/useImages";
+
+const Forgot_password = () => {
+
+    const { forgotPassword } = useImages()
+
     return (
         <div>
-            Forgot Password
+            <Formik
+                initialValues={{
+                    Email: "",
+                }}
+                validationSchema={Yup.object({
+                    Email: Yup.string().required("El email es requerido"),
+                })}
+                onSubmit={(values, actions) => {
+                    console.log(values);
+                    forgotPassword(values);
+                }}
+            >
+                {({ handleChange, handleSubmit }) => (
+                    <Form onSubmit={handleSubmit}>
+                        <Field name="Email" placeholder="Email..."></Field>
+                        <ErrorMessage name="Email" />
+                        <button className="cyanBtn" type="submit">Enviar</button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     )
 };
