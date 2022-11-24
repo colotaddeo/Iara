54a8fbe4fcbb1f9dae1879db22e20eaf2afdbeca
Benzagon <gonbenzaquen@gmail.com>
2022-11-24 19:50:05 -0300

    feat: added basic html for main body

--- Front-End/src/components/pages/landingPageV2/Landing.jsx
+++ Front-End/src/components/pages/landingPageV2/Landing.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 //import components
 import LandingNav from "./landingComponents/LandingNav";
+import MainBody from "./landingComponents/MainBody"
 import './scss/style.css';
 
 const Landing = () => {
@@ -9,8 +10,7 @@ const Landing = () => {
     return (
         <>
             <LandingNav></LandingNav>
-            <br />
-            <p>This is body</p>
+            <MainBody></MainBody>
         </>
     );
 };
---
+++ Front-End/src/components/pages/landingPageV2/landingComponents/MainBody.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const MainBody = () => {
+
+    return (
+        <>
+            <div className="landBody">
+                <div className="landBody__left">
+                    <h1>
+                        <span>Enter a new techological era</span> <br />
+                        <span>with </span> <span>IARA</span>
+                    </h1>
+                    <h3>Improve diagnostics completely free of charge</h3>
+                    <button className="land_clearBtn">Start Now</button>
+                </div>
+
+                <div className="landBody__right">
+                    
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default MainBody;
