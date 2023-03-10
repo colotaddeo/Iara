5d42502321a799e1a982b23b4f0da970572637fe
Benzagon <gonbenzaquen@gmail.com>
2023-03-09 21:59:09 -0300

    fix: folder name changed
    Also modified docker file trying to fix it

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({include: "**/*.jsx",})]
+  plugins: [react({include: "**/*.jsx",})],
+  server: {
+    host:"0.0.0.0"
+  }
 })
