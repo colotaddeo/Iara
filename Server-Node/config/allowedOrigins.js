42033f6cb0ee7883f203f723c185a05e4cf787ff
Benzagon <gonbenzaquen@gmail.com>
2023-04-10 20:48:44 -0300

    feat: backendFlask dockerized

--- Server-Node/config/allowedOrigins.js
+++ Server-Node/config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://localhost:4000',
     'http://localhost:8000',
     'http://127.0.0.1:5173',
-    'http://localhost:8081'
+    'http://localhost:8081',
+    'http://127.0.0.1:8000'
 ]
 
 export default allowedOrigins   
