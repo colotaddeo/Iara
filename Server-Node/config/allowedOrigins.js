d235086a1899b1d00e50a5622d7fb2dfb3acb254
Benzagon <gonbenzaquen@gmail.com>
2023-04-12 14:30:26 -0300

    test: added 2 new origins to cors

--- Server-Node/config/allowedOrigins.js
+++ Server-Node/config/allowedOrigins.js
@@ -4,7 +4,8 @@ const allowedOrigins = [
     'http://localhost:8000',
     'http://127.0.0.1:5173',
     'http://localhost:8081',
-    'http://127.0.0.1:8000'
+    'http://127.0.0.1:8000',
+    'http://host.docker.internal:8000'
 ]
 
 export default allowedOrigins   
